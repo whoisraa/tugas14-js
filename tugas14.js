7f21a18b02ae2ad1266acb4519152e528f59b095
whoisraa <raainvictus@gmail.com>
2021-11-06 15:38:52 +0700

    tugas 14 js

---
+++ tugas14.js
@@ -0,0 +1,10 @@
+function pembagian() {
+    let bagi = 37 / 23; // 23 merupakan tanggal hari
+    console.log(bagi);
+
+    console.log(Math.round(bagi));
+    console.log(Math.ceil(bagi));
+    console.log(Math.floor(bagi));
+}
+
+pembagian();
